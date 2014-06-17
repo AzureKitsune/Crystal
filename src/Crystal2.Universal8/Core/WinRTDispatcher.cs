338dd2c6427e7fca6f8bda3ae26dbc076536cb8f
Amrykid <amrykid@gmail.com>
2014-06-17 12:48:11 -0500

    "Fixed" some dispatcher issues.

--- src/Crystal2.Universal8/Core/WinRTDispatcher.cs
+++ src/Crystal2.Universal8/Core/WinRTDispatcher.cs
@@ -7,17 +7,28 @@ using Windows.UI.Core;
 
 namespace Crystal2.Core
 {
-    public sealed class WinRTDispatcher: IUIDispatcher
+    public sealed class WinRTDispatcher : IUIDispatcher
     {
         private CoreDispatcher dispatcherObject = null;
         internal WinRTDispatcher()
         {
-            dispatcherObject = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
+            SetDispatcher();
+        }
+
+        private void SetDispatcher()
+        {
+            try
+            {
+                dispatcherObject = Windows.ApplicationModel.Core.CoreApplication.MainView.CoreWindow.Dispatcher;
+            }
+            catch (InvalidOperationException) { }
         }
 
         public Task RunAsync(Action callback)
         {
             if (callback == null) throw new ArgumentNullException("callback");
+
+            if (dispatcherObject == null) SetDispatcher();
 
             return dispatcherObject.RunAsync(CoreDispatcherPriority.Normal, new DispatchedHandler(() => callback())).AsTask();
         }
