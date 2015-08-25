7b0287d24c2e7bd62bd6f876eaf9eec5fbbe6bef
Amrykid <amrykid@gmail.com>
2015-08-25 08:50:54 -0500

    Created a PageExtensions class in Crystal3.UI. First method is one I used in CookieQuiz to bind a property on a ViewModel with the current page's pagetitle attribute.

---
+++ src/Crystal3/UI/PageExtensions.cs
@@ -0,0 +1,28 @@
+﻿using Crystal3.Navigation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel.Channels;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+
+namespace Crystal3.UI
+{
+    public static class PageExtensions
+    {
+        public static void BindPageTitleToViewModelProperty(this Page page, string propertyName)
+        {
+            if (string.IsNullOrWhiteSpace(propertyName)) throw new ArgumentNullException("propertyName");
+
+            page.SetBinding(NavigationAttributes.PageTitleProperty, new Windows.UI.Xaml.Data.Binding()
+            {
+                Path = new PropertyPath(propertyName),
+                UpdateSourceTrigger = UpdateSourceTrigger.PropertyChanged,
+                FallbackValue = page.GetValue(NavigationAttributes.PageTitleProperty) as string
+            });
+        }
+    }
+}
