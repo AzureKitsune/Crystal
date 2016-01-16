7b389f837bec31f9ac8c440d520ee0f59fa40c53
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-01-16 10:03:00 -0600

    New converters!

---
+++ src/Crystal3/UI/Converters/CollectionNullOrEmptyToVisibilityConverter.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Crystal3.UI.Converters
+{
+    public class CollectionNullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value == null)
+            {
+                return Visibility.Collapsed;
+            }
+            else
+            {
+                if (value is IEnumerable<object>)
+                    return ((IEnumerable<object>)value).Count() > 0 ? Visibility.Visible : Visibility.Collapsed;
+            }
+
+            return Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/Crystal3/UI/Converters/InverseCollectionNullOrEmptyToVisibilityConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace Crystal3.UI.Converters
+{
+    public class InverseCollectionNullOrEmptyToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var returnValue = ((Visibility)new CollectionNullOrEmptyToVisibilityConverter().Convert(value, targetType, parameter, language)) == Visibility.Visible ? Visibility.Collapsed : Visibility.Visible;
+            return returnValue;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
