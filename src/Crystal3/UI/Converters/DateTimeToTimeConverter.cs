2988b40d7b5d7d698d2dee041847572a1b9f7a33
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-02-20 21:50:27 -0600

    Added a DateTime To Time string converter.

---
+++ src/Crystal3/UI/Converters/DateTimeToTimeConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Crystal3.UI.Converters
+{
+    //https://msdn.microsoft.com/en-us/library/az4se3k1(v=vs.110).aspx
+    public class DateTimeToTimeConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value != null && value is DateTime)
+            {
+                var dateTime = (DateTime)value;
+
+                return dateTime.ToString("t");
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
