79d195b90d3352d9999dfc8261f1285d87b2692f
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-04-30 15:51:19 -0500

    More extension methods.

---
+++ src/Crystal3/Utilities/CollectionExtensions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crystal3.Utilities
+{
+    public static class CollectionExtensions
+    {
+        public static void AddRange<T>(this ICollection<T> collection, IEnumerable<T> items)
+        {
+            if (collection == null) throw new ArgumentNullException("collection");
+            if (items == null) throw new ArgumentNullException("items");
+
+            foreach (var item in items)
+                collection.Add(item);
+        }
+    }
+}
