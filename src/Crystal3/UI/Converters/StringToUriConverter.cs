87fb2b1cc864fc8bb1cbbce129362dd2ac8ca0d4
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-08-02 16:01:13 -0500

    Added a StringToUriConverter.

---
+++ src/Crystal3/UI/Converters/StringToUriConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace Crystal3.UI.Converters
+{
+    public class StringToUriConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (!string.IsNullOrWhiteSpace(((string)value)))
+                return new Uri((string)value);
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
