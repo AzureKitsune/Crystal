fc0c770f2e4644f1feb553c70de3bda988af05cc
Alexander Mitchell-Robinson <amrykid@gmail.com>
2016-09-21 21:17:23 -0500

    Added a way to render fragments.

---
+++ src/Crystal3/Navigation/FragmentManager.cs
@@ -0,0 +1,39 @@
+﻿using Crystal3.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace Crystal3.Navigation
+{
+    public static class FragmentManager
+    {
+        private static Dictionary<Type, Type> fragmentViewList = new Dictionary<Type, Type>();
+
+        public static void RegisterFragmentView<T, V>() where V : FrameworkElement where T : ViewModelFragment
+        {
+            if (!fragmentViewList.ContainsKey(typeof(T)))
+                fragmentViewList.Add(typeof(T), typeof(V));
+        }
+
+        public static void UnregisterFragmentView<T, V>() where V : FrameworkElement where T : ViewModelFragment
+        {
+            if (fragmentViewList.ContainsKey(typeof(T)))
+                fragmentViewList.Remove(typeof(T));
+        }
+
+        public static Type ResolveFragmentView<T>() where T : ViewModelFragment
+        {
+            return ResolveFragmentView(typeof(T));
+        }
+        public static Type ResolveFragmentView(Type fragmentType)
+        {
+            if (fragmentViewList.ContainsKey(fragmentType))
+                return fragmentViewList[fragmentType] as Type;
+            else
+                return null;
+        }
+    }
+}
---
+++ src/Crystal3/UI/FragmentContentViewer.xaml.cs
@@ -0,0 +1,75 @@
+﻿using Crystal3.Model;
+using Crystal3.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace Crystal3.UI
+{
+    public sealed partial class FragmentContentViewer : UserControl
+    {
+        public FragmentContentViewer()
+        {
+            this.InitializeComponent();
+        }
+
+        public static readonly DependencyProperty FragmentProperty = DependencyProperty.Register("Fragment", typeof(ViewModelFragment), typeof(FragmentContentViewer), new PropertyMetadata(null, HandleFragmentPropertyChanged));
+
+        public ViewModelFragment Fragment
+        {
+            get { return (ViewModelFragment)GetValue(FragmentProperty); }
+            set { SetValue(FragmentProperty, value); }
+        }
+
+        private static void HandleFragmentPropertyChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var viewer = d as FragmentContentViewer;
+            if (viewer != null)
+            {
+                HandleDisplay(viewer, e.OldValue as ViewModelFragment, e.NewValue as ViewModelFragment);
+            }
+        }
+
+        private static void HandleDisplay(FragmentContentViewer viewer, ViewModelFragment oldValue, ViewModelFragment newValue)
+        {
+            if (oldValue != null)
+                oldValue.Dispose();
+
+            if (newValue != null)
+            {
+                var viewType = FragmentManager.ResolveFragmentView(newValue.GetType());
+
+                if (viewType != null)
+                {
+                    var view = Activator.CreateInstance(viewType) as FrameworkElement;
+
+                    var newFragment = newValue as ViewModelFragment;
+
+                    view.DataContext = newFragment;
+                    newFragment.UI.SetUIElement(view);
+
+                    viewer.PART_ContentPresenter.Content = view;
+
+                    newFragment.Invoke(viewer.DataContext as ViewModelBase, null);
+                }
+            }
+            else
+            {
+                viewer.PART_ContentPresenter.Content = null;
+            }
+        }
+    }
+}
